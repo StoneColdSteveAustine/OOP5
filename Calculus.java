1521d6571ad79d412e11402d0b6334f6e19b3f8a
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-07-25 20:37:29 +0600

    Update Calculus.java

--- Calculus.java
+++ Calculus.java
@@ -3,10 +3,10 @@ import java.util.function.BiFunction;
 public class Main {
     public static void main(String[] args) {
         Object[] data = {left_value, oper, right_value};
-        System.out.println(Calc_block(data));
+        System.out.println(calc_block(data));
     }
 
-    public static Object Calc_block(Object[] data) {
+    public static Object calc_block(Object[] data) {
         Object left_value = data[0];
         Object oper = data[1];
         Object right_value = data[2];
